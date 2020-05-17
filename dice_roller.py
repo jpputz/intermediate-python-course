1981b8df244a8fe3fa0967c5f03a3874d57165bc
unknown <janphillipputz@gmail.com>
2020-05-17 23:00:02 +0200

    Now rolling more than one dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,14 @@
+import random
+
 def main():
-    print("You rolled a die")
-  #print('You rolled a die')
+    dice_rolls = 10
+    dice_sum = 0
+    for i in range(dice_rolls):
+        roll = random.randint(1, 6)
+        dice_sum = dice_sum + roll
+        print(f"Your roll has been a {roll}")
+    print(dice_sum)
+
 
 if __name__== "__main__":
   main()
